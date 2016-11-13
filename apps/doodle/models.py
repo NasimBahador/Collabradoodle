61781c9b1a02735fe670e0b09cfb0edbff98b42f
Ben Swanson <bentswanson@gmail.com>
2016-11-13 00:10:09 -0800

    login/registration validation complete, fixes model managers

--- apps/doodle/models.py
+++ apps/doodle/models.py
@@ -4,16 +4,47 @@ from django.core.exceptions import ObjectDoesNotExist
 import bcrypt, re
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-# Create your models here.
+
 class UserManager(models.Manager):
     def validate_reg(self, request):
-        pass
-    def validate_login(self, request):
-        pass
-    def validate_inputs(self, request):
-        pass
+        errors = self.validate_inputs(request)
 
-class DoodleManager(object):
+        if errors:
+            return (False, errors)
+
+        pw_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+
+        user = self.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'], pw_hash=pw_hash)
+
+        return (True, user)
+
+    def validate_login(self, request):
+        try:
+            user = User.objects.get(email=request.POST['email'])
+            password = request.POST['password'].encode()
+            if bcrypt.hashpw(password, user.pw_hash.encode()):
+                return (True, user)
+
+        except ObjectDoesNotExist:
+            pass
+
+        return (False, ["Invalid login."])
+
+    def validate_inputs(self, request):
+        errors = []
+
+        if not request.POST['first_name']:
+            errors.append('First name cannot be blank.')
+        if not EMAIL_REGEX.match(request.POST['email']):
+            errors.append('Invalid email.')
+        if request.POST['password'] < 8:
+            errors.append('Password must be at least 8 characters.')
+        if request.POST['password'] != request.POST['confirm']:
+            errors.append('Password and password confirm must match.')
+
+        return errors
+
+class DoodleManager(models.Manager):
     def post_doodle(self, request):
         pass
     def destroy_doodle(self, request):
@@ -28,8 +59,12 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    objects = UserManager()
+
 class Doodle(models.Model):
     content = models.CharField(max_length = 300)
     doodle_creator = models.ForeignKey(User, related_name = 'user_doodle')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = DoodleManager()
--- apps/doodle/views.py
+++ apps/doodle/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from models import User, Doodle
 
-# Create your views here.
 def session_check(request):
     pass
 
@@ -8,19 +9,31 @@ def index(request):
     return render(request,'index.html')
 
 def login_reg(request):
-    pass
+    if request.POST['action'] == 'register':
+        result = User.objects.validate_reg(request)
 
-def print_errors(request):
-    pass
+    elif request.POST['action'] == 'login':
+        result = User.objects.validate_login(request)
 
-def log_in_user(request):
-    pass
+    if result[0] == False:
+        print_errors(request, result[1])
+        return redirect('/')
+
+    return log_user_in(request, result[1])
+
+
+def print_errors(request, error_list):
+    for error in error_list:
+        messages.add_message(request, messages.INFO, error)
+
+def log_user_in(request, user):
+    return redirect('wall')
 
 def doodle(request):
     pass
 
 def wall(request):
-    pass
+    return render(request, 'wall.html')
 
 def logout(request):
     pass
